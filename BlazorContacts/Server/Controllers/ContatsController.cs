2b9ece8a3ae69789d497c025840f93d6de20c361
mi <mi@DESKTOP-OVUF872>
2023-10-22 04:42:28 +0100

    Add project files.

---
+++ BlazorContacts/Server/Controllers/ContatsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorContacts.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatsController : ControllerBase
+    {
+    }
+}
